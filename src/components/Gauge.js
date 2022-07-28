4f67f0d28aaab602e37967569380a3c2e71ea88e
mjyinseoul <mjyinseoul@gmail.com>
2022-07-28 21:19:02 +0100

    chage transform style by calculated values

--- src/components/Gauge.js
+++ src/components/Gauge.js
@@ -4,6 +4,8 @@ import GaugeStyles from '../styles/Gauge.module.css'
 
 export const Gauge = () => {
     const [gaugeValue, setGaugeValue] = useState([])
+    const [percentage, setPercentage] = useState('')
+    const [rotate, setRotate] = useState('')
 
     useEffect(() => {
         getDefaultValue()
@@ -15,17 +17,27 @@ export const Gauge = () => {
             const res = await axios.get('https://widgister.herokuapp.com/challenge/frontend?fixed=1')
 
             setGaugeValue(res.data)
+            calculateValues(res.data)
         }catch(err){
             console.log(err)
         }
+    }
+
+    // calculate percentage and rotate for the display  
+    const calculateValues = (data) => {
+        setPercentage(((data.value / data.max) * 100).toFixed(2))
+        setRotate(((data.value / data.max) / 2).toFixed(2))
     }
 
     return (
         <>
         <div className={GaugeStyles.container}>
             <div className={GaugeStyles.body}>
-                <div className={GaugeStyles.fill}>
-                    
+                <div className={GaugeStyles.fill} 
+                    style={{transform:`rotate(${rotate}turn)`}}>
+                    <span style={{transform:`rotate(${rotate * -1}turn)`}}>
+                        {percentage}%
+                    </span>
                 </div>
                 <div className={GaugeStyles.cover}>{gaugeValue.unit} {gaugeValue.value}
                 </div>
