821951659dbbc67d5ecdafa1c02dc24124a9ff2f
mjyinseoul <mjyinseoul@gmail.com>
2022-07-28 23:16:50 +0100

    showErrorMsg and updateValues

--- src/components/UpdateValues.js
+++ src/components/UpdateValues.js
@@ -4,18 +4,21 @@ import UpdateStyles from '../styles/UpdateValues.module.css'
 export const UpdateValues = ({ gaugeValues, setGaugeValues, setShowUpdatePopUp }) => {
     const [updateVal, setUpdateVal] = useState(gaugeValues)
     const [activeButton, setActiveButton] = useState(false)
+    const [showErrorMsg, setShowErrorMsg] = useState(false)
 
     useEffect(() => {
         // when min or value is bigger than max, inactive the confirm button
         if(updateVal.min > updateVal.value || updateVal.value > updateVal.max || updateVal.min > updateVal.max){
             setActiveButton(false)
+            setShowErrorMsg(true)
         }else{
             setActiveButton(true)
+            setShowErrorMsg(false)
         }
     },[updateVal])
 
     const styles = {
-        button : {
+        button : { // make confirm button blue when the state is true 
             background : activeButton ? '#1F609E' : '#66686C'
         }
     }
@@ -24,13 +27,19 @@ export const UpdateValues = ({ gaugeValues, setGaugeValues, setShowUpdatePopUp }
         return +val.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1').substr(0,3)
     }
 
+    const updateGaugeValues = () => {
+        setGaugeValues(updateVal)
+        setShowUpdatePopUp(false)
+    }
+
     return (
         <div className={UpdateStyles.wrapper}>
            <div className={UpdateStyles.container}>
-                <h3>Update Gauge Value</h3>
+                <h3>Update Gauge Values</h3>
+                {showErrorMsg && <p>Min or Value can not be bigger than Max</p>}
                 <div className={UpdateStyles.main}>
                     <label htmlFor="min">Min</label>
-                    <input type="number" placeholder="min" id="min" value={updateVal.min} 
+                    <input type="number" id="min" value={updateVal.min} 
                         onInput={(e)=>e.target.value = numberRegExp(e.target.value)} 
                         onChange={(e)=>setUpdateVal({...updateVal, min : +e.target.value})} />
                     <label htmlFor="value">Value</label>
@@ -44,7 +53,8 @@ export const UpdateValues = ({ gaugeValues, setGaugeValues, setShowUpdatePopUp }
                 </div>
                 <div className={UpdateStyles.buttons}>
                     <button type="button" onClick={()=>setShowUpdatePopUp(false)}>Close</button>
-                    <button type="button" style={styles.button} >Confirm</button>
+                    <button type="button" style={styles.button} 
+                        disabled={activeButton ? false : true} onClick={()=>updateGaugeValues()}>Confirm</button>
                 </div>
             </div>
         </div>
