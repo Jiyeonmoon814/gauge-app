4d2630e66712bfe273466a84cf9c6a15e8234cf5
mjyinseoul <mjyinseoul@gmail.com>
2022-07-28 22:26:28 +0100

    set,get update values with css

--- src/components/UpdateValues.js
+++ src/components/UpdateValues.js
@@ -9,7 +9,17 @@ export const UpdateValues = ({ gaugeValues, setGaugeValues, setShowUpdatePopUp }
         <div className={UpdateStyles.wrapper}>
            <div className={UpdateStyles.container}>
                 <h3>Update Gauge Value</h3>
-                
+                <div className={UpdateStyles.main}>
+                    <label htmlFor="min">Min</label>
+                    <input type="number" placeholder="min" id="min" value={updateVal.min} 
+                        onChange={(e)=>setUpdateVal({...updateVal, min : +e.target.value})} />
+                    <label htmlFor="value">Value</label>
+                    <input type="number" id="value" value={updateVal.value} 
+                        onChange={(e)=>setUpdateVal({...updateVal, value : +e.target.value})} />
+                    <label htmlFor='max'>Max</label>
+                    <input type="number" id="max" value={updateVal.max} 
+                        onChange={(e)=>setUpdateVal({...updateVal, max : +e.target.value})} />
+                </div>
                 <div className={UpdateStyles.buttons}>
                     <button type="button" onClick={()=>setShowUpdatePopUp(false)}>Close</button>
                     <button type="button">Confirm</button>
